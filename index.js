4248271dabd7b4bce1390f5f119990b393824c4e
Angela Shen <angelayshen@gmail.com>
2023-05-06 21:20:34 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(
+        (driver) => driver.toLowerCase() === name.toLowerCase()
+    );
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(
+        (driver) => driver.slice(0,1) === name.slice(0,1)
+    );
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(
+        (driver) => driver.name === name
+    );
+}
